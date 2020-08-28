81394a46dc178e5c3f40db54e341836f6dadfdb5
Saurabh Singh <singhsaurabh920@gmail.com>
2020-08-28 04:46:26 +0000

    velocity added

--- src/main/java/org/worldbuild/core/config/CoreConfiguration.java
+++ src/main/java/org/worldbuild/core/config/CoreConfiguration.java
@@ -2,6 +2,8 @@ package org.worldbuild.core.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.exception.VelocityException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
@@ -11,8 +13,10 @@ import org.springframework.http.client.InterceptingClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
@@ -46,5 +50,15 @@ public class CoreConfiguration {
         //restTemplate.setErrorHandler(new RestTemplateErrorHandler());
         //restTemplate.setInterceptors(Arrays.asList(new RestTemplateInterceptor()));
         return restTemplate;
+    }
+
+    @Bean
+    public VelocityEngine getVelocityEngine() throws VelocityException, IOException {
+        VelocityEngine velocityEngine = new VelocityEngine();
+        Properties props = new Properties();
+        props.put("resource.loader", "class");
+        props.put("class.resource.loader.class",  "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+        velocityEngine.init(props);
+        return velocityEngine;
     }
 }
--- src/main/java/org/worldbuild/core/config/SchedulerConfig.java
+++ src/main/java/org/worldbuild/core/config/SchedulerConfig.java
@@ -2,10 +2,12 @@ package org.worldbuild.core.config;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 @Log4j2
+@EnableAsync
 @Configuration
 public class SchedulerConfig implements SchedulingConfigurer {
 	
